604c489d4ee46f7781744701234d927afca36139
aimer-w <blakevids1@gmail.com>
2024-09-17 13:41:35 -0600

    Create assignment5.java

---
+++ assignment5.java
@@ -0,0 +1,9 @@
+public class assignment5 {
+  public static void main (String[] args) {
+    System.out.println("This is assignment 5");
+  }
+
+  public int add (int a, int b){
+  // Fill in your code here
+  }
+}
